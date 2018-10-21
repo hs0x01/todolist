191cdbc96e0ff3afcb5c83a6467ecf54156ca276
hs0x01 <kumamon0x01@gmail.com>
2018-10-21 15:21:52 +0900

    追加

--- todolist-front/link.bat
+++ todolist-front/link.bat
@@ -5,7 +5,7 @@ set SELF_DIR=%~dp0
 cd %~dp0
 
 IF "%APACHE_HOME%" == "" (
-	echo "APACHE_HOMEÇ™ãÛÇ≈Ç∑ÅB"
+	echo "There is no APACHE_HOME."
 	exit /b 1
 ) ELSE (
 	mklink /d "%APACHE_HOME%\htdocs\js" "%SELF_DIR%\js"
